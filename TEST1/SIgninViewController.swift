d77f0d6af864b4caca15ba2d53fc154cde844e93
Gunjan Raval <grawal72@gmail.com>
2020-04-18 17:48:14 +0530

    signing

---
+++ TEST1/SIgninViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SIgninViewController.swift
+//  TEST1
+//
+//  Created by Gunjan Raval on 18/04/20.
+//  Copyright © 2020 Gunjan Raval. All rights reserved.
+//
+
+import UIKit
+
+class SIgninViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func InitMethod(str : String) -> String {
+        return str
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
