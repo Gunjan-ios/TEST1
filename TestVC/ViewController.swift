76ce2562fe7e2ab6dc9455548f551eb7df7431b8
Gunjan Raval <grawal72@gmail.com>
2020-04-18 13:48:38 +0530

    add VC file

---
+++ TestVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestVC
+//
+//  Created by Gunjan Raval on 18/04/20.
+//  Copyright © 2020 Gunjan Raval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestVCTests/TestVCTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestVCTests.swift
+//  TestVCTests
+//
+//  Created by Gunjan Raval on 18/04/20.
+//  Copyright © 2020 Gunjan Raval. All rights reserved.
+//
+
+import XCTest
+@testable import TestVC
+
+class TestVCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
