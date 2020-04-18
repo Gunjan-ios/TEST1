1af2d5b477ae27c38b7fa2165645e05d31dc187e
Gunjan Raval <grawal72@gmail.com>
2020-04-18 12:26:59 +0530

    Start

---
+++ TEST1/HelloWorld.swift
@@ -0,0 +1,18 @@
+//
+//  HelloWorld.swift
+//  TEST1
+//
+//  Created by Gunjan Raval on 18/04/20.
+//  Copyright © 2020 Gunjan Raval. All rights reserved.
+//
+
+import Foundation
+
+public class HelloWorld {
+    let hello = "Hello"
+    
+    public init() {}
+    public func hello(to whom: String) -> String {
+        return "Hello \(whom)"
+    }
+}
---
+++ TEST1/TEST1.h
@@ -0,0 +1,19 @@
+//
+//  TEST1.h
+//  TEST1
+//
+//  Created by Gunjan Raval on 18/04/20.
+//  Copyright © 2020 Gunjan Raval. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TEST1.
+FOUNDATION_EXPORT double TEST1VersionNumber;
+
+//! Project version string for TEST1.
+FOUNDATION_EXPORT const unsigned char TEST1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TEST1/PublicHeader.h>
+
+
